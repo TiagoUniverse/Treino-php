399cdfb0ffdf8a9cd46affd43fcdea3c5a33f1ae
Tiago César da Silva Lopes <71237410+TiagoUniverse@users.noreply.github.com>
2023-04-17 19:32:31 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+    <main>
+      <h1>Curso de Análise e desenvolvimento de sistemas do Senac!</h1>
+      <h2>Graduação focada no mercado de trabalho e atual. </h2>
+      <h3> Segue abaixo as principais características do curso </h3>
+    </main>
+
+    
+  <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
+  </body>
+</html>
